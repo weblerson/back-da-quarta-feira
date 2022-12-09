7bd10eb85ec0f692e04684857a1cf2a9e5162b29
weblerson <zonejr4@gmail.com>
2022-12-08 20:08:55 -0400

    auth password and time data return

--- config.py
+++ config.py
@@ -1,15 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from pydantic import BaseModel
 from decouple import config
 
 from typing import List
 
-__all__: List[str, ...] = ['Config', 'FastAPI']
+__all__: List[str] = ['Config', 'FastAPI', 'Request', 'GlobalArea']
 
 
 class Config:
     def __init__(self) -> None:
         self.__app: FastAPI
-        if config('PROD', cast=bool, default=False):
+        if config('PROD', cast=bool, default=True):
             self.__app = FastAPI()
         else:
             self.__app = FastAPI(docs_url=None, redoc_url=None)
@@ -17,3 +18,7 @@ class Config:
     @property
     def app(self) -> FastAPI:
         return self.__app
+
+
+class GlobalArea(BaseModel):
+    region: str
--- main.py
+++ main.py
@@ -1,3 +1,41 @@
 from config import *
 
+from decouple import config
+
+import datetime
+import pytz
+
 app: FastAPI = Config().app
+
+
+@app.post('/time')
+def get_time(request: Request, area: GlobalArea):
+    if not request.headers.get('AuthDistaste') == config('AUTH_PASSWORD', cast=str):
+        return {
+            'success': False,
+            'body': 'Invalid auth password'
+        }
+
+    u: datetime.datetime = datetime.datetime.utcnow()
+    u = u.replace(tzinfo=pytz.utc)
+
+    try:
+        info: datetime.datetime = u.astimezone(pytz.timezone(area.region))
+
+        return {
+            'success': True,
+            'body': {
+                'hora': info.hour,
+                'minuto': info.minute,
+                'segundo': info.second,
+                'dia': info.day,
+                'mês': info.month,
+                'ano': info.year
+            }
+        }
+
+    except Exception as e:
+        return {
+            'success': False,
+            'body': f'Invalid input: {e}'
+        }
